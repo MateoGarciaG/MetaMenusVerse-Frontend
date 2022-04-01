1771998e86067b40c574e53e1f45394c77c97df8
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2022-04-02 00:49:17 +0200

    feat: :sparkles: Add pagination component

--- src/components/groupCard/groupCard.js
+++ src/components/groupCard/groupCard.js
@@ -6,8 +6,9 @@ import groupCardHtml from "./groupCard.html?raw";
 import "./groupCard.module.scss";
 import { html, render } from 'lit-html';
 import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
+import { Pagination } from "../pagination/pagination";
 
-let cards = [Card("Pedro", 27.3), Card("MArio", 12.3), Card("MArio", 12.3), ];
+let cards = [Card("Pedro", 27.3), Card("MArio", 12.3), Card("arturo", 12.3), Card("jose", 12.3), Card("sergio", 12.3), Card("carlos", 12.3), Card("juan", 12.3), ];
 
 
 export const GroupCard = () => {
@@ -17,7 +18,11 @@ export const GroupCard = () => {
 
   const showCardsContainter = document.querySelector(".show-cards");
 
-  render(html`${cards.map((card) => unsafeHTML(card))}`, showCardsContainter);
+  // render(html`${cards.map((card) => unsafeHTML(card))}`, showCardsContainter);
+
+  const paginationContainer = document.querySelector(".pagination-cards");
+
+  Pagination(showCardsContainter, paginationContainer, cards);
 
   // return html`
   // ${unsafeHTML(groupCardHtml)}
